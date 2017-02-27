7118783e8f7363d067f277747affcb8879fbbd9a
MrGold <dgold@sjnma.org>
2017-02-27 08:17:49 -0600

    initial commit of simple

--- PersistantLuckySum.py
+++ PersistantLuckySum.py
@@ -1,1 +1,26 @@
- 
+ '''
+Ask for 3 ints
+print their sum, however while the number is
+not evenly divisble by 3 and 5 continue asking.  Then
+the number is not added and
+the number to the right is not added
+
+'''
+
+firstnumber = int(raw_input("first number"))
+secondnumber = int(raw_input("second number"))
+thirdnumber = int(raw_input("third number"))
+
+#is each number divisble by 3 or 5
+if(firstnumber % 3 == 0 and firstnumber%5 ==0):
+	print(thirdnumber)
+elif(secondnumber % 3 == 0 and secondnumber%5 ==0):
+	print(firstnumber)
+elif(thirdnumber % 3 == 0 and thirdnumber%5 ==0):
+	print(firstnumber+secondnumber)
+else:
+    print(firstnumber+secondnumber+thirdnumber)
+
+
+
+#print their appropriate sum
---
+++ WhileNotTheRightAnswer.py
@@ -0,0 +1,7 @@
+'''
+Ask a student how they are doing today.
+Until they respond with an answer we decide in class,
+Keep asking
+'''
+
+
