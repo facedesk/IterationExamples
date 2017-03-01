48e313911c15c6caaa57f1d0b5b3e577c8ca7339
MrGold <dgold@sjnma.org>
2017-03-01 09:51:08 -0600

    added random number generator example

---
+++ randomNumber.py
@@ -0,0 +1,26 @@
+from random import randrange
+
+result = randrange(0, 10)
+print (result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+while(result !=5):
+    result = randrange(0, 10)
+    print (result)
+'''
